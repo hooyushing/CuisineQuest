77bf909de5d9d4344cd349532ef2edaba4a0ea48
hooyushing <94732346+hooyushing@users.noreply.github.com>
2024-05-19 17:44:48 +0800

    Added local file for functionality

--- CuisineQuest.py
+++ CuisineQuest.py
@@ -5,6 +5,12 @@ import logging
 import telegram
 from telegram.ext import *
 from telegram import *
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn import preprocessing
+import random
 
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
@@ -17,6 +23,8 @@ TOKEN = '7113953543:AAGsP6zbVPR1kM8AwbU0Jjy7_jt0W5Jlh-A'
 updater = Updater(token = TOKEN, use_context = True)
 
 dp = updater.dispatcher
+
+hawkers = pd.read_csv('/Users/DELL1/Downloads/Telegram Desktop/Updated List Of Hawker Centers.csv')
 
 
 def start(update, context):
@@ -36,7 +44,8 @@ def location(update, context):
     button2 = KeyboardButton('South')
     button3 = KeyboardButton('East')
     button4 = KeyboardButton('West')
-    keyboard = ReplyKeyboardMarkup([[button1], [button2], [button3], [button4]], resize_keyboard=True, one_time_keyboard=True)
+    button5 = KeyboardButton('Central')
+    keyboard = ReplyKeyboardMarkup([[button1], [button2], [button3], [button4], [button5]], resize_keyboard=True, one_time_keyboard=True)
     
     update.message.reply_text('Choose a direction to find restaurants:', reply_markup=keyboard)
 
@@ -44,12 +53,36 @@ def handle_message(update, context):
     text = update.message.text
     if text == 'North':
         update.message.reply_text('Searching for restaurants in the North!')
+        updated = np.array(hawkers.loc[hawkers["Location"] == "North"])
+        size = updated.shape
+        rows = random.randint(0, size[0] - 1)
+        update.message.reply_text("Your food place is: " + updated[rows, 0] + " (" + updated[rows, 2] + ")")
     elif text == 'South':
         update.message.reply_text('Searching for restaurants in the South!')
-    elif text == 'West':
+        updated = np.array(hawkers.loc[hawkers["Location"] == "South"])
+        size = updated.shape
+        rows = random.randint(0, size[0] - 1)
+        update.message.reply_text("Your food place is: " + updated[rows, 0] + " (" + updated[rows, 2] + ")")
+    elif text == "West":
         update.message.reply_text('Searching for restaurants in the West!')
+        updated = np.array(hawkers.loc[hawkers["Location"] == "West"])
+        size = updated.shape
+        rows = random.randint(0, size[0] - 1)
+        update.message.reply_text("Your food place is: " + updated[rows, 0] + " (" + updated[rows, 2] + ")")
     elif text == 'East':
         update.message.reply_text('Searching for restaurants in the East!')
+        updated = np.array(hawkers.loc[hawkers["Location"] == "East"])
+        size = updated.shape
+        rows = random.randint(0, size[0] - 1)
+        
+        update.message.reply_text("Your food place is: " + updated[rows, 0] + " (" + updated[rows, 2] + ")")
+    elif text == 'Central':
+        update.message.reply_text('Searching for restaurants in the Central')
+        updated = np.array(hawkers.loc[hawkers["Location"] == "Central"])
+        size = updated.shape
+        rows = random.randint(0, size[0] - 1)
+        
+        update.message.reply_text("Your food place is: " + updated[rows, 0] + " (" + updated[rows, 2] + ")")
     else:
         update.message.reply_text('Invalid option! Please use the /location command to choose again.')
         
@@ -57,7 +90,7 @@ def handle_message(update, context):
 
 
 
-dggsc
+
 
 dp.add_handler(CommandHandler("start", start))
 dp.add_handler(CommandHandler("features", feature_command))
