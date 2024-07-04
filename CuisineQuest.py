b697060e88f241b389c772252d6fbdf62b86670b
hooyushing <94732346+hooyushing@users.noreply.github.com>
2024-07-04 17:37:58 +0800

    Fixed ratings

--- CuisineQuest.py
+++ CuisineQuest.py
@@ -16,44 +16,46 @@ import re
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 
+# Initialize logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s, level = logging.INFO')
-
+# Define the bot token
 TOKEN = '7113953543:AAGsP6zbVPR1kM8AwbU0Jjy7_jt0W5Jlh-A'
 
-updater = Updater(token = TOKEN, use_context = True)
-
+# Initialize the updater and dispatcher
+updater = Updater(token=TOKEN, use_context=True)
 dp = updater.dispatcher
 
+# Load the hawkers CSV file
 hawkers = pd.read_csv('/Users/DELL1/Downloads/Telegram Desktop/Orbital 12 Jun CSV File.csv')
 
-START, FEATURE, CUISINE, DIETARY, LOCATION, RATING = range(6)
+# Define conversation states
+START, FEATURE, CUISINE, DIETARY, LOCATION, RATING_SELECT, RATING_INPUT = range(7)
 
-
+# Start command handler
 def start(update: Update, context: CallbackContext) -> int:
     update.message.reply_text("Hello! Welcome to CuisineQuest bot. Type /features to see available instructions")
     return START
 
+# Features command handler
 def feature_command(update: Update, context: CallbackContext) -> int:
     features_text = (
-       "/Location - Choose a location of your liking and discover the best restaurants\n"
-       "/dietary_preference - Filter restaurants based on dietary preference\n"
-       "/Cuisine - Filter restaurants based on cuisine!\n"
-       "/Ratings - Rate a restaurant!"
+        "/Location - Choose a location of your liking and discover the best restaurants\n"
+        "/dietary_preference - Filter restaurants based on dietary preference\n"
+        "/Cuisine - Filter restaurants based on cuisine!\n"
+        "/Ratings - Review your favourite restaurants!"
     )
     update.message.reply_text(features_text)
     return FEATURE
 
+# Dietary preference handler
 def dietary_preference(update: Update, context: CallbackContext) -> int:
     button1 = KeyboardButton('Vegan')
     button2 = KeyboardButton('Halal')
     button3 = KeyboardButton('Vegetarian')
-    
-    
     keyboard = ReplyKeyboardMarkup([[button1], [button2], [button3]], resize_keyboard=True, one_time_keyboard=True)
-    
-    update.message.reply_text('Please specify if you have any dietary preferences', reply_markup=keyboard)      
-    return DIETARY  
+    update.message.reply_text('Please specify if you have any dietary preferences', reply_markup=keyboard)
+    return DIETARY
 
 def handle_dietary_preference(update: Update, context: CallbackContext) -> int:
     text = update.message.text
@@ -64,12 +66,13 @@ def handle_dietary_preference(update: Update, context: CallbackContext) -> int:
             update.message.reply_text(f'No {text} eateries')
         else:
             rows = random.randint(0, updated.shape[0] - 1)
-            update.message.reply_text("Your eatery is: " + updated[rows, 3] + ", ratings: " + updated[rows, 13])
+            rated = str(updated[rows, 13])
+            update.message.reply_text("Your eatery is: " + updated[rows, 3] + " (Rated: " + rated + "/5)")
     else:
         update.message.reply_text('Invalid option! Please use the /dietary_preference command to choose again.')
-    return ConversationHandler.END    
-    
+    return ConversationHandler.END
 
+# Cuisine handler
 def cuisine(update: Update, context: CallbackContext) -> int:
     button1 = KeyboardButton('Asian')
     button2 = KeyboardButton('Western')
@@ -79,9 +82,9 @@ def cuisine(update: Update, context: CallbackContext) -> int:
     button6 = KeyboardButton('Korean')
     button7 = KeyboardButton('Japanese')
     button8 = KeyboardButton('Others (Click for surprise!)')
-    
-    keyboard = ReplyKeyboardMarkup([[button1], [button2], [button3], [button4], [button5], [button6], [button7], [button8]], resize_keyboard=True, one_time_keyboard=True)
-    
+    keyboard = ReplyKeyboardMarkup(
+        [[button1], [button2], [button3], [button4], [button5], [button6], [button7], [button8]],
+        resize_keyboard=True, one_time_keyboard=True)
     update.message.reply_text('Choose a cuisine to enjoy!', reply_markup=keyboard)
     return CUISINE
 
@@ -94,16 +97,24 @@ def handle_cuisine(update: Update, context: CallbackContext) -> int:
             update.message.reply_text(f'No eateries found with {text} cuisine')
         else:
             rows = random.randint(0, updated.shape[0] - 1)
-            update.message.reply_text("Your eatery is: " + updated[rows, 3] + ", ratings: " + updated[rows, 13])
+            rated = str(updated[rows, 13])
+            update.message.reply_text("Your eatery is: " + updated[rows, 3] + " (Rated: " + rated + "/5)")
     else:
-        update.message.reply_text('Invalid option! Please use the /cuisine command to choose again.')    
-    return ConversationHandler.END    
+        update.message.reply_text('Invalid option! Please use the /cuisine command to choose again.')
+    return ConversationHandler.END
 
-
-
-
+# Find eatery handler
 def find_eatery(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text('These are the available MRT stations you can key in: Admiralty, Aljunied, Ang Mo Kio, Bartley, Bayfront, Bedok, Bishan, Boon Keng, Boon Lay, Bras Basah, Braddell, Buangkok, Bugis, Buona Vista, Changi Airport, Choa Chu Kang, Chinese Garden, Chinatown, City Hall, Clarke Quay, Clementi, Commonwealth, Dover, Dhoby Ghaut, Eunos, Esplanade, Expo, Farrer Park, HarbourFront, Havelock, Hougang, Joo Koon, Jurong East, Kallang, Kembangan, Keppel, Khatib, Kovan, Kranji, Lakeside, Lavender, Little India, Lorong Chuan, Marina Bay, Marina South, Marine Parade, Marine Terrace, Marsiling, Maxwell, Mountbatten, Novena, Orchard, Pasir Ris, Paya Lebar, Pioneer, Potong Pasir, Promenade, Punggol, Queenstown, Raffles Place, Redhill, Sembawang, Sengkang, Shenton Way, Siglap, Simei, Somerset, Stadium, Suntec City, Tampines, Tanah Merah, Tanjong Katong, Tanjong Pagar, Toa Payoh, Tiong Bahru, VivoCity, Woodlands, Yew Tee, Yishun')
+    update.message.reply_text(
+        'These are the available MRT stations you can key in: Admiralty, Aljunied, Ang Mo Kio, Bartley, Bayfront, '
+        'Bedok, Bishan, Boon Keng, Boon Lay, Bras Basah, Braddell, Buangkok, Bugis, Buona Vista, Changi Airport, '
+        'Choa Chu Kang, Chinese Garden, Chinatown, City Hall, Clarke Quay, Clementi, Commonwealth, Dover, Dhoby Ghaut, '
+        'Eunos, Esplanade, Expo, Farrer Park, HarbourFront, Havelock, Hougang, Joo Koon, Jurong East, Kallang, '
+        'Kembangan, Keppel, Khatib, Kovan, Kranji, Lakeside, Lavender, Little India, Lorong Chuan, Marina Bay, '
+        'Marina South, Marine Parade, Marine Terrace, Marsiling, Maxwell, Mountbatten, Novena, Orchard, Pasir Ris, '
+        'Paya Lebar, Pioneer, Potong Pasir, Promenade, Punggol, Queenstown, Raffles Place, Redhill, Sembawang, '
+        'Sengkang, Shenton Way, Siglap, Simei, Somerset, Stadium, Suntec City, Tampines, Tanah Merah, Tanjong Katong, '
+        'Tanjong Pagar, Toa Payoh, Tiong Bahru, VivoCity, Woodlands, Yew Tee, Yishun')
     update.message.reply_text('Please enter your closest MRT Station name.')
     return LOCATION
 
@@ -127,32 +138,58 @@ def handle_location(update: Update, context: CallbackContext) -> int:
             update.message.reply_text(f'No eateries found near {text}')
         else:
             rows = random.randint(0, updated.shape[0] - 1)
-            update.message.reply_text("Your eatery is: " + updated[rows, 3] + ", ratings: " + updated[rows, 13])
+            rated = str(updated[rows, 13])
+            update.message.reply_text("Your eatery is: " + updated[rows, 3] + " (Rated: " + rated + "/5)")
     else:
         update.message.reply_text('Invalid location. Please enter a valid location.')
-    return ConversationHandler.END    
+    return ConversationHandler.END
 
-
-
-
+# Rate restaurants handler
 def rate_restaurants(update: Update, context: CallbackContext) -> int:
     update.message.reply_text('Please enter the name of the restaurant you wish to rate.')
-    return RATING
+    return RATING_SELECT
 
 def handle_ratings(update: Update, context: CallbackContext) -> int:
     keywords = update.message.text
     pattern = re.compile(keywords, re.IGNORECASE)
-    matching_restaurants = df[df['Restaurant Name'].apply(lambda x: bool(pattern.search(x)))]
-    matching_names = matching_restaurants['Restaurant Name'].tolist()
+    matching_restaurants = hawkers[hawkers['name'].apply(lambda x: bool(pattern.search(x)))]
+    matching_names = matching_restaurants['name'].tolist()
 
     if matching_names:
-        update.message.reply_text("Matching Restaurants:\n" + "\n".join(matching_names))
+        buttons = [
+            [InlineKeyboardButton(name, callback_data=name)] for name in matching_names
+        ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        update.message.reply_text("Select the restaurant you want to rate:", reply_markup=reply_markup)
+        return RATING_INPUT
     else:
-        update.message.reply_text("No matching restaurants found.")
-    return ConversationHandler.END      
+        update.message.reply_text("No matching restaurants found. Please send the name again.")
+        return RATING_SELECT
 
+def handle_rating(update: Update, context: CallbackContext) -> int:
+    query = update.callback_query
+    query.answer()
+    context.user_data['restaurant'] = query.data
+    query.edit_message_text(text=f"Selected option: {query.data}\n\nPlease send the rating (0-5):")
+    return RATING_INPUT
 
+def save_rating(update: Update, context: CallbackContext) -> int:
+    restaurant = context.user_data['restaurant']
+    rating = update.message.text
+    
 
+    if rating.isdigit() and 0 <= int(rating) <= 5:
+        total = hawkers.loc[hawkers['name'] == restaurant, 'rating'] * hawkers.loc[hawkers['name'] == restaurant, 'visited']
+        hawkers.loc[hawkers['name'] == restaurant, 'visited'] = hawkers.loc[hawkers['name'] == restaurant, 'visited'] + 1
+        hawkers.loc[hawkers['name'] == restaurant, 'rating'] = (int(rating) + total)/hawkers.loc[hawkers['name'] == restaurant, 'visited']
+        hawkers.to_csv('/Users/DELL1/Downloads/Telegram Desktop/Orbital 12 Jun CSV File.csv', index=False)
+        update.message.reply_text(f"Rating for {restaurant} updated to {float(hawkers.loc[hawkers['name'] == restaurant, 'rating'])}.\n\nThank you for contributing!")
+    else:
+        update.message.reply_text("Invalid rating. Please send a number between 0 and 5.")
+
+    return ConversationHandler.END
+
+# Define the conversation handler with states
 conv_handler = ConversationHandler(
     entry_points=[CommandHandler('start', start)],
     states={
@@ -161,20 +198,29 @@ conv_handler = ConversationHandler(
             CommandHandler('cuisine', cuisine),
             CommandHandler('dietary_preference', dietary_preference),
             CommandHandler('location', find_eatery),
-            CommandHandler('Review', rate_restaurants)
+            CommandHandler('Ratings', rate_restaurants)
         ],
         CUISINE: [MessageHandler(Filters.text & ~Filters.command, handle_cuisine)],
         DIETARY: [MessageHandler(Filters.text & ~Filters.command, handle_dietary_preference)],
         LOCATION: [MessageHandler(Filters.text & ~Filters.command, handle_location)],
-        RATING: [MessageHandler(Filters.text & ~Filters.command, handle_ratings)]
+        RATING_SELECT: [
+            MessageHandler(Filters.text & ~Filters.command, handle_ratings),
+        ],
+        RATING_INPUT: [
+            CallbackQueryHandler(handle_rating),
+            MessageHandler(Filters.text & ~Filters.command, save_rating)
+        ]
     },
     fallbacks=[CommandHandler('start', start)]
 )
 
+# Add the conversation handler to the dispatcher
 dp.add_handler(conv_handler)
 
+# Start the bot
 updater.start_polling()
 updater.idle()
+
 
 
 
